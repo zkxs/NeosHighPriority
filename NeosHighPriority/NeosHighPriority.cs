13dfec39636304a8f31aba803a43aaada6ebd4f1
Michael Ripley <zkxs00@gmail.com>
2021-12-07 17:15:01 -0600

    initial commit

---
+++ NeosHighPriority/NeosHighPriority.cs
@@ -0,0 +1,23 @@
+﻿using NeosModLoader;
+using System.Diagnostics;
+
+namespace NeosHighPriority
+{
+    public class NeosHighPriority : NeosMod
+    {
+        public override string Name => "NeosHighPriority";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosHighPriority";
+
+        public override void OnEngineInit()
+        {
+            using (Process p = Process.GetCurrentProcess())
+            {
+                var oldPriority = p.PriorityClass;
+                p.PriorityClass = ProcessPriorityClass.High;
+                Msg($"Changed process priority from {oldPriority} to {p.PriorityClass}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# NeosHighPriority
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that sets the Neos process to high priority.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [NeosHighPriority.dll](https://github.com/zkxs/NeosHighPriority/releases/latest/download/NeosHighPriority.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
